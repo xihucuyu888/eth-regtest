ecc823fcfe34794e89cdfd987fc51aba4f3a044d
xihucuyu888 <xihucuyuzjuer@gmail.com>
2020-07-09 16:56:44 +0800

    add scripts

---
+++ main.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd /opt/eth
+
+pm2 start start-eth.sh
+
---
+++ start-eth.sh
@@ -0,0 +1,1 @@
+geth --mine --miner.threads=1 --datadir /opt/eth/gethDataDir/ --networkid 123 --rpc --rpcaddr 0.0.0.0 --rpcport=8547 --rpcapi="db,eth,net,web3,personal,web3" --allow-insecure-unlock --miner.etherbase=0xEc67A59e54A393b702c7EcCe1faca731E4f0e601
